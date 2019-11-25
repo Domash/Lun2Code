83b18fda025e6229b765b0fddd3bb065b9a352be
Domash <35339409+Domash@users.noreply.github.com>
2019-11-25 22:03:02 +0300

    [master]: tuned database, using MySql

---
+++ Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lun2Code.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lun2Code
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+			
+			services.AddScoped<IUsersRepository, UsersRepository>();
+			services.AddDbContext<UsersContext>(settings
+				=> settings.UseMySql(Configuration.GetConnectionString("DefaultConnection"))); // add connection name
+						
+			services.AddIdentity<User, IdentityRole>(settings => {
+				settings.Password.RequiredLength         = 4;
+				settings.Password.RequireDigit           = false;
+				settings.Password.RequireLowercase       = false;
+				settings.Password.RequireUppercase       = false;
+				settings.Password.RequireNonAlphanumeric = false;
+			}).AddEntityFrameworkStores<UsersContext>();
+			
+
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+			app.UseAuthentication();
+			app.UseStatusCodePages();
+			app.UseHttpsRedirection();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
