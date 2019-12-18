d7fc0daa74d49342eca97e3e36e574593be9e58d
denisdomashevich <domashevich2014@gmail.com>
2019-12-18 23:11:23 +0300

    [master]: working with CF API

--- Controllers/ContestsController.cs
+++ Controllers/ContestsController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Lun2Code.Contest;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,11 +7,20 @@ namespace Lun2Code.Controllers
 {
     public class ContestsController : Controller
     {
+
+        public static List<Contest.Contest> Contests { get; set; }
+        
         public IActionResult Index()
         {
             var cf = new Codeforces(DateTime.Now);
 
-            var kek = cf.GetContestsList();
+            if (Contests == null)
+            {
+                Contests = cf.GetContestsList();
+                Contests.Reverse();
+            }
+
+            ViewData.Model = Contests;
             
             return View();
         }
